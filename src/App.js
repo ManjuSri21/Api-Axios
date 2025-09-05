394ba799975a42a701cd8f4b6d6fab0a8fd00c18
MANJU SRI D <155386140+ManjuSri21@users.noreply.github.com>
2025-09-05 16:52:00 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Database from './components/Database';
-import CRUD from './components/CRUD';
 import Users from './components/Users'
-import Photos from './components/Photos'
 
 function App() {
   return (
     <div className='container-fluid'>
     <Users/>  <br/><hr/>
-    <CRUD/>   <br/><hr/>
+    <Database/>   <br/><hr/>
     </div>
   )
 }
